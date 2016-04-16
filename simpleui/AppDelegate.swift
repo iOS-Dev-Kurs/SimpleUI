c97baef4bd7c24a0afa451c99358205d3274982d
Nils Fischer <n.fischer@viwid.com>
2016-04-16 11:50:04 +0200

    initial commit

---
+++ simpleui/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  SimpleUI
+//
+//  Created by Nils Fischer on 06.03.15.
+//  Copyright (c) 2015 Nils Fischer. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+}
+
