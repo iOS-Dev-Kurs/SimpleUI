c7692826f2ca4dcdfe318f9d8d0f865cbfb8b8f4
julian libor <gb004@r214c2.ad.uni-heidelberg.de>
2016-04-19 16:44:16 +0200

    2/3 erledigt
    Testen hat noch nicht funktioniert

--- simpleui/ViewController.swift
+++ simpleui/ViewController.swift
@@ -9,6 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var count: Int = 0
+    var bmi: Float = 0
+    
+    @IBOutlet var counter_label: UILabel!
+    
+    
+    @IBAction func plus_one(sender: AnyObject) {
+        count += 1
+        counter_label.text = String(count)
+    }
 
+    
+    @IBAction func reset_label(sender: AnyObject) {
+        count = 0
+        counter_label.text = String(count)
+ 
+    }
+    
+    @IBAction func minus_one(sender: AnyObject) {
+        count -= 1
+        counter_label.text = String(count)
+
+    }
+    
+    
+    @IBOutlet weak var gewicht_label: UILabel!
+    @IBOutlet weak var gewicht_slider: UISlider!
+    @IBAction func gewicht_slider(sender: AnyObject) {
+        gewicht_label.text = "Dein Gewicht: \(gewicht_slider.value)"
+        bmi = gewicht_slider.value/(laenge_slider.value*laenge_slider.value)
+        bmi_label.text = "Dein BMI: \(bmi)"
+    }
+    
+    
+    @IBOutlet weak var laenge_label: UILabel!
+    @IBOutlet weak var laenge_slider: UISlider!
+    @IBAction func laenge_slider_change(sender: AnyObject) {
+        laenge_label.text = "Deine Größe: \(laenge_slider.value)"
+        bmi = gewicht_slider.value/(laenge_slider.value*laenge_slider.value)
+        bmi_label.text = "Dein BMI: \(bmi)"
+    }
+    
+    
+    @IBOutlet weak var bmi_label: UILabel!
 }
-
