e79da3cfff7839d880520032f96d585452448ee6
Kleimaier <vy442@r214c6.ad.uni-heidelberg.de>
2016-04-20 11:59:31 +0200

    TestBMI

--- simpleui/ViewController.swift
+++ simpleui/ViewController.swift
@@ -10,5 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var massTextField: UITextField!
+    @IBOutlet weak var heightTextField: UITextField!
+    @IBOutlet weak var BMILabel: UILabel!
+
+    
+    @IBAction func BMIButtonPressed(sender: AnyObject) {
+        if let mass = massTextField.text {
+            if let height = heightTextField.text{
+                let ma = Double(mass)
+                let hei = Double(height)
+                let BMI = ma! / (hei!*hei!)
+                BMILabel.text = "Your BMI: \(BMI)"
+            }else{
+                BMILabel.text = "Your BMI: failure:your too fat;)"
+            }
+
+        }else{
+            BMILabel.text = "Your BMI: failure:your too fat;)"
+        }
+    }
 }
 
