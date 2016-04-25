d66b740da8429f2d9ff24d7f6f9d3fe319b7b8e1
Lucas Moeller <lucasmoeller@lucass-macbook-air.local>
2016-04-25 14:39:35 +0200

    first commit

--- simpleui/ViewController.swift
+++ simpleui/ViewController.swift
@@ -9,6 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var gewicht: UITextField!
+    @IBOutlet var groesse: UITextField!
+    @IBOutlet var bmi: UILabel!
+    @IBAction func button(sender: UIButton) {
+        if let weight = gewicht.text  {
+            if let length = groesse.text {
+                if let weight2 = Double(weight){
+                    if let length2 = Double(length){
+                        
+                        bmi.text = String(round(100*(weight2/(length2*length2)))/100)
+                    }
+                    else{bmi.text = "inkorrekte Eingabe"}
+                }
+                else{bmi.text = "inkorrekte Eingabe"}
+            }
+            else{bmi.text = "inkorrekte Eingabe"}
+        }
+        else{bmi.text = "inkorrekte Eingabe"}
+                
+                
+                
+        
+                
+        
+        
+    }
+    
+    
 }
 
