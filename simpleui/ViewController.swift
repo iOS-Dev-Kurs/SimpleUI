7ec66cf0445cfa3f6912d1e8da9681c3972e08d2
Ellie92 <Ellie@dhcp660-223.laptop-wlc.uni-heidelberg.de>
2017-04-10 13:14:36 +0200

    BMI Rechner

--- simpleui/ViewController.swift
+++ simpleui/ViewController.swift
@@ -10,5 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+ 
+    @IBOutlet weak var nameGewicht: UITextField!
+    @IBOutlet weak var nameGröße: UITextField!
+    
+    @IBOutlet weak var bmiLabel: UILabel!
+    
+    @IBAction func nameButton(_ sender: Any) {
+        guard let gewicht = nameGewicht.text
+            else {
+                return
+        }
+        guard let größe = nameGröße.text
+            else {
+                return
+        }
+        
+        guard let m = Double(gewicht)
+            else {
+        return
+        }
+        guard let l = Double(größe)
+            else {
+        return
+        }
+        
+        
+        let bmi = m/pow(l,2)
+        
+    
+        bmiLabel.text = String(format: "Der BMI beträgt %.2f", bmi)
+       
+
+    
+        
+    }
 }
 
